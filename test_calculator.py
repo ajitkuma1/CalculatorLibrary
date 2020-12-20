6b602bef786c7835d381155d21d63989f7145696
ajitkuma1 <ajitesh.wipro@gmail.com>
2020-12-20 18:29:49 +0530

    Changed as per PEP8

--- test_calculator.py
+++ test_calculator.py
@@ -1,12 +1,8 @@
 """ Unit test for calculator functions"""
-
 import calculator
-
 class TestCalculator:
-
-
     def test_addtion(self):
         assert 5 == calculator.addNumbers(2, 3)
-    
+
     def test_subtraction(self):
         assert -4 == calculator.subtractNumber(1, 5)
