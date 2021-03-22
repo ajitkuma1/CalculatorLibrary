293177b3518d340be63f2279968e5782864f3a09
ajitkuma1 <ajitesh.wipro@gmail.com>
2021-03-23 01:07:51 +0530

    Changed test_calculator

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,13 @@
 """ Unit test for calculator functions"""
 import calculator
 class TestCalculator:
+
+    
     def test_addtion(self):
         assert 5 == calculator.addNumbers(2, 3)
 
     def test_subtraction(self):
         assert -4 == calculator.subtractNumber(1, 5)
     
-
     def test_multiplication(self):
-        assert 100 == calculator.multiplyNumbers(10,10)
-    
+        assert 100 == calculator.multiplyNumbers(10, 10)
